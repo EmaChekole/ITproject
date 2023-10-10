e152191855ad0bc5869900b77816695c57355cf4
shreejal11 <shreejalshrestha10@gmail.com>
2023-10-10 16:23:21 +1030

    added function to run query in index.js.

--- myExpressApp/routes/index.js
+++ myExpressApp/routes/index.js
@@ -1,15 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/buildings', function(req, res, next) {
-
-  console.log("buildings has been called");
-  //Connect to the database
+/* Function to connect to DB and use query*/
+function useQuery(query,req,res) {
+  
   req.pool.getConnection( function(err,connection) {
     if (err) {
       console.log(err);
@@ -17,7 +11,6 @@ router.get('/buildings', function(req, res, next) {
       return;
     }
 
-    var query = "SELECT Building_id, Building_name FROM building";
     connection.query(query, function(err, rows, fields) {
       connection.release(); // release connection
       if (err) {
@@ -27,7 +20,20 @@ router.get('/buildings', function(req, res, next) {
       }
       res.json(rows);
     });
+    
   });
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
+
+router.get('/buildings', function(req, res, next) {
+  console.log("buildings has been called");
+  var query = "SELECT Building_id, Building_name FROM building";
+  useQuery(query,req,res);
+});
+
 
 module.exports = router;
