9fe4a013ae4ab0411e53d00808f0c3092487ee7c
Davidbendik <147581746+Davidbendik@users.noreply.github.com>
2023-11-05 06:18:28 +0000

    update

--- myExpressApp/public/javascript/scripts.js
+++ myExpressApp/public/javascript/scripts.js
@@ -3,21 +3,22 @@
 /*---------------FILLING ON BEHALF OF SOMEONE ELSE----------------*/
 function toggleTextbox() {
     var checkBox = document.getElementById('onBehalfCheckbox');
-    var inputField = document.getElementById('e13581');
+    var input = document.getElementById('e13581');
     if (checkBox.checked) {
-        inputField.style.display = 'block';
-        inputField.readOnly = false;
+        input.style.display = 'block';
+        input.readOnly = false;
     } else {
-        inputField.style.display = 'none';
-        inputField.value = ''; 
+        input.style.display = 'none';
+        input.value = ''; 
     }
 }
 /*------------------------------------------------------------------*/
 
-document.getElementById('commit_11296').addEventListener('click', function() {
-    alert('You have submitted the form');
-    window.location.reload();
-  });
+
+
+
+
+
 
 
 
@@ -35,18 +36,21 @@ document.getElementById('e13602').addEventListener('input', function() {
         results.innerHTML = '';
         var inputValue = this.value.toLowerCase();
     
-        for(let subject of subjectAreas) {
-            if (subject.toLowerCase().includes(inputValue)) {
-                let div = document.createElement('div');
-                div.innerText = subject;
-                div.addEventListener('click', function() {
-                    document.getElementById('e13602').value = subject;
+        subjectAreas.forEach(function(sbj) {
+            var searchTerm = inputValue.toLowerCase();
+
+            if (sbj.toLowerCase().indexOf(searchTerm) !== -1) {
+                var resultDiv = document.createElement('div');
+                resultDiv.textContent = sbj;
+                resultDiv.onclick = function() {
+                    var inputField = document.getElementById('e13602');
+                    inputField.value = sbj;
                     results.style.display = 'none';
                     subSectionInput.disabled = false;
-                });
-                results.appendChild(div);
+                };
+                results.appendChild(resultDiv);
             }
-        }
+        });
 
         results.style.display = results.children.length > 0 ? 'block' : 'none';
     });
@@ -55,7 +59,7 @@ document.getElementById('e13602').addEventListener('input', function() {
 document.getElementById('subSection').addEventListener('input', function() {
     var results = document.getElementById('subjectResults');
     var selectedSubjectArea = document.getElementById('e13602').value;  
-    var inputFieldUsers = document.getElementById('e13611');
+    var inputUsers = document.getElementById('e13611');
     var inputValue = document.getElementById('subSection').value.toLowerCase();
 
     var subjects = [];
@@ -80,13 +84,14 @@ document.getElementById('subSection').addEventListener('input', function() {
                         document.getElementById('subSection').value = subject;
                         results.style.display = 'none';
 
-                        // Clear the rooms list and visual cues when a new subject is selected
-                        document.getElementById('room_list').innerHTML = '';
-                        var roomResultsDivs = document.getElementById('room_results').querySelectorAll('div');
-                        roomResultsDivs.forEach(function(div) {
-                            div.style.backgroundColor = '';  
-                            div.style.border = '';
-                        });
+                        //Clear the rooms list
+                        var clearRoomList = document.getElementById('room_list');
+                        clearRoomList.innerHTML = '';
+                        var roomDivs = document.getElementById('room_results').getElementsByTagName('div');
+                        for (var i = 0; i < roomDivs.length; i++) {
+                            roomDivs[i].style.backgroundColor = '';
+                            roomDivs[i].style.border = '';
+                        }
 
                         fetch("/studentCount", {
                             method: "POST",
@@ -97,11 +102,11 @@ document.getElementById('subSection').addEventListener('input', function() {
                         })
                         .then(response => response.json())
                         .then(data => {
-                            inputFieldUsers.value = data[0].student_enrol;
+                            inputUsers.value = data[0].student_enrol;
                         })
-                        .catch(error => console.error('Error:', error));
+                        
 
-                        // Fetch the rooms for the selected subject
+                        //get the rooms for the selected subject
                         fetch("/roomsForSubject", {
                             method: "POST",
                             headers: {
@@ -113,7 +118,7 @@ document.getElementById('subSection').addEventListener('input', function() {
                         .then(data => {
                             displayRooms(data);
                         })
-                        .catch(error => console.error('Error:', error));
+                      
                     });
                     results.appendChild(div);
                 }
@@ -137,30 +142,29 @@ function displayRooms(rooms) {
         div.addEventListener('click', function() {
             var roomList = document.getElementById('room_list');
             var roomText = room.Building_name + " - " + room.room_type + " - Room " + room.room_number;
-        
-            // Check if the room is already in the list
+
             var matchingLi = Array.from(roomList.querySelectorAll('li')).find(li => li.innerText.startsWith(roomText));
             
             if (matchingLi) {
-                // If room is already in the list, remove it and revert the visual cues
+                //If room is already in the list remove it
                 roomList.removeChild(matchingLi);
                 div.style.backgroundColor = '';  
                 div.style.border = '';
             } else {
-                // If room is not in the list, add it and add the visual cues
+                //If room is not in the list add it
                 div.style.backgroundColor = '#e0e0e0';  
                 div.style.border = '1px solid #000';
         
                 var li = document.createElement('li');
                 li.innerText = roomText;
         
-                // Add a remove button
+                //remove button
                 var removeBtn = document.createElement('button');
                 removeBtn.innerText = "Remove";
                 removeBtn.addEventListener('click', function() {
                     roomList.removeChild(li);
         
-                    // Revert the visual cue when the room is removed from the list
+          
                     div.style.backgroundColor = '';  
                     div.style.border = '';
                 });
@@ -176,13 +180,13 @@ function displayRooms(rooms) {
 }
 
 document.getElementById('e13602').addEventListener('change', function() {
-    // Clear the subSection input field
+    //Clear subSection input
     document.getElementById('subSection').value = '';
     
-    // Clear the rooms list
+    //Clear rooms list
     document.getElementById('room_list').innerHTML = '';
     
-    // If you have any visual cues or styles for rooms in room_results, you'll also need to clear them
+
     var roomResultsDivs = document.getElementById('room_results').querySelectorAll('div');
     roomResultsDivs.forEach(function(div) {
         div.style.backgroundColor = '';  
@@ -206,7 +210,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const softwareSupplierInput = document.getElementById("e13595");
     const obtainingMediaTextarea = document.getElementById("e13587");
     const newAppPrompt = document.getElementById('newAppPrompt');
-    const createNewAppBtn = document.getElementById("createNewApp"); // New button
+    const createNewAppBtn = document.getElementById("createNewApp");
 
     const applications = [];
     let xhttp = new XMLHttpRequest();
@@ -228,38 +232,44 @@ document.addEventListener("DOMContentLoaded", function() {
         const inputValue = this.value.toLowerCase();
         resultsDiv.innerHTML = '';
 
-        for (let app of applications) {
-            if (app.software_name.toLowerCase().includes(inputValue)) {
-                let div = document.createElement('div');
-                div.innerText = app.software_name + (app.version ? " (" + app.version + ")" : "");  // Display version if available
-                div.addEventListener('click', function() {
-                    // Concatenate software name with version (if available) for display
-                    const softwareDisplayText = app.software_name + (app.version ? " (" + app.version + ")" : "");
+        var allApps = applications;
+        for (var i = 0; i < allApps.length; i++) {
+            var currentApp = allApps[i];
+            var search = inputValue.toLowerCase();
+            if (currentApp.software_name.toLowerCase().indexOf(search) !== -1) {
+                var resultDiv = document.createElement('div');
+                var versionText = currentApp.version ? " (" + currentApp.version + ")" : "";
+                resultDiv.textContent = currentApp.software_name + versionText;
                 
-                    appInput.value = softwareDisplayText;
-                    appNameInput.value = softwareDisplayText;  // Update appNameInput with software name and version
-                
-                    resultsDiv.style.display = 'none';
-                    newAppPrompt.style.display = 'none';
-                
-                    if (app.software_name === "Other") {
-                        appNameInput.removeAttribute("disabled");
-                        softwareSupplierInput.removeAttribute("disabled");
-                        obtainingMediaTextarea.removeAttribute("disabled");
-                        appNameInput.value = "";
-                        softwareSupplierInput.value = "";
-                        obtainingMediaTextarea.value = "";
-                    } else {
-                        appNameInput.setAttribute("disabled", "disabled");
-                        softwareSupplierInput.removeAttribute("disabled");
-                        obtainingMediaTextarea.removeAttribute("disabled");
-                        softwareSupplierInput.value = app.software_supplier;
-                    
-                    }
-                });
-                resultsDiv.appendChild(div);
+                resultDiv.onclick = function(appCopy) {
+                    return function() {
+                        var displayText = appCopy.software_name + (appCopy.version ? " (" + appCopy.version + ")" : "");
+                        
+                        appInput.value = displayText;
+                        appNameInput.value = displayText; 
+                        
+                        resultsDiv.style.display = 'none';
+                        newAppPrompt.style.display = 'none';
+                        
+                        if (appCopy.software_name === "Other") {
+                            appNameInput.removeAttribute("disabled");
+                            softwareSupplierInput.removeAttribute("disabled");
+                            obtainingMediaTextarea.removeAttribute("disabled");
+                            appNameInput.value = "";
+                            softwareSupplierInput.value = "";
+                            obtainingMediaTextarea.value = "";
+                        } else {
+                            appNameInput.setAttribute("disabled", "disabled");
+                            softwareSupplierInput.removeAttribute("disabled");
+                            obtainingMediaTextarea.removeAttribute("disabled");
+                            softwareSupplierInput.value = appCopy.software_supplier;
+                        }
+                    };
+                }(currentApp);
+                resultsDiv.appendChild(resultDiv);
             }
         }
+        
 
         resultsDiv.style.display = resultsDiv.children.length > 0 ? 'block' : 'none';
 
@@ -270,16 +280,19 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    document.addEventListener('click', function(event) {
-        if (!event.target.closest('#e13600')) {
+    document.addEventListener('click', function(evt) {
+
+        if (!evt.target.closest('#e13600')) {
+
             resultsDiv.style.display = 'none';
-            if (appInput.value.trim() == "") {
+            
+            if (appInput.value.trim() === "") {
                 newAppPrompt.style.display = 'none';
             }
         }
     });
 
-    // Handle the "Create New Application" button click
+    //"Create New Application" button click
     createNewAppBtn.addEventListener('click', function(event) {
         event.preventDefault();
     
@@ -289,7 +302,7 @@ document.addEventListener("DOMContentLoaded", function() {
         appNameInput.value = "";
         softwareSupplierInput.value = "";
         obtainingMediaTextarea.value = "";
-        appInput.value = "Other";  // Set the value to "Other" to indicate a new application
+        appInput.value = "Other";
         resultsDiv.style.display = 'none';
         newAppPrompt.style.display = 'none';
     });
